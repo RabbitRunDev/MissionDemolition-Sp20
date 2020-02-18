43e69a103805c97b06daad52e16aed01150d877e
Aidan Cushing <aac202@rutgers.edu>
2020-02-17 21:41:49 -0500

    Slingshot Updates

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -6,24 +6,20 @@ public class Slingshot : MonoBehaviour
 {
 
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject prefabProjectile;
+    public GameObject projectile;
+    public bool aimingMode;
+    public float velocityMult = 8f;
+
+    private Rigidbody projectileRigidBody;
 
     private void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        launchPos = launchPointTrans.position;
     }
 
     void OnMouseEnter()
@@ -34,5 +30,46 @@ public class Slingshot : MonoBehaviour
     void OnMouseExit()
     {
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+
+        projectileRigidBody = projectile.GetComponent<Rigidbody>();
+        projectileRigidBody.isKinematic = true;
+    }
+
+    void Update()
+    {
+        if (!aimingMode)
+        {
+            return;
+        }
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidBody.isKinematic = false;
+            projectileRigidBody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
     }
 }
