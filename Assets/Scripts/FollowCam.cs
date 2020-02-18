bee0fe1785370fbb023d2e15ad379ebf19db549d
Aidan Cushing <aac202@rutgers.edu>
2020-02-17 21:54:51 -0500

    Camera Fixes

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -8,6 +8,9 @@ public class FollowCam : MonoBehaviour
 
     public float camZ;
 
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     void Awake()
     {
         camZ = this.transform.position.z;
@@ -18,7 +21,11 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return;
 
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
